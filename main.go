57af0631eacf112e12c463c20ba6fc1c86884a75
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2019-08-21 15:21:41 +0430

    Added mutex for safe operations ABSOLUTELY PRE-PRE-ALPHA HAVEN'T TESTED IT EVEN ONCE

--- main.go
+++ main.go
@@ -10,16 +10,27 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 )
 
-var Rules []Rule
+var Rules CSafeRule
 var ConfigFileName = "rules.json"
-var SimultaneousConnections = make([]int, 0)
+var SimultaneousConnections CSafeConnections
 var Verbose = false
 
 const Version = "0.2.0 / Build 3"
+
+type CSafeConnections struct {
+	SimultaneousConnections []int
+	mu                      sync.Mutex
+}
+
+type CSafeRule struct {
+	Rules []Rule
+	mu    sync.Mutex
+}
 
 type Rule struct {
 	Listen       uint16
@@ -61,36 +72,42 @@ func main() {
 	if err != nil {
 		panic("Cannot read the config file. (Parse Error) " + err.Error())
 	}
-	Rules = conf.Rules
-	SimultaneousConnections = make([]int, len(Rules))
+	Rules.Rules = conf.Rules
+	SimultaneousConnections.SimultaneousConnections = make([]int, len(Rules.Rules))
 
 	//Start listeners
-	for index := range Rules {
+	for index := range Rules.Rules {
 		go func(i int) {
-			if Rules[i].Quota < 0 { //If the quota is already reached why listen for connections?
+			Rules.mu.Lock()            //Lock it and clone it
+			loopRule := Rules.Rules[i] //Clone it
+			Rules.mu.Unlock()          //Let the other goroutines use it
+			if loopRule.Quota < 0 {    //If the quota is already reached why listen for connections?
 				return
 			}
-			fmt.Println("Forwarding from", Rules[i].Listen, "port to", Rules[i].Forward)
-			ln, err := net.Listen("tcp", ":"+strconv.Itoa(int(Rules[i].Listen))) //Listen on port
+			fmt.Println("Forwarding from", loopRule.Listen, "port to", loopRule.Forward)
+			ln, err := net.Listen("tcp", ":"+strconv.Itoa(int(loopRule.Listen))) //Listen on port
 			if err != nil {
 				panic(err)
 			}
 
 			for {
 				conn, err := ln.Accept() //The loop will be held here
-				if Rules[i].Quota < 0 {
-					fmt.Println("Quota reached for port", Rules[i].Forward, "pointing to", Rules[i].Forward)
+				Rules.mu.Lock()          //Lock the mutex to just read the quota
+				if Rules.Rules[i].Quota < 0 {
+					Rules.mu.Unlock()
+					fmt.Println("Quota reached for port", loopRule.Forward, "pointing to", loopRule.Forward)
 					if err == nil {
 						_ = conn.Close()
 					}
 					saveConfig(conf)
 					break
 				}
+				Rules.mu.Unlock()
 				if err != nil {
 					println("Error on accepting connection:", err.Error())
 					continue
 				}
-				go handleRequest(conn, i)
+				go handleRequest(conn, i, loopRule)
 			}
 		}(index)
 	}
@@ -119,7 +136,9 @@ func main() {
 }
 
 func saveConfig(config Config) {
-	config.Rules = Rules
+	Rules.mu.Lock() //Lock to clone the rules
+	config.Rules = Rules.Rules
+	Rules.mu.Unlock()
 	b, err := json.Marshal(config)
 	if err != nil {
 		fmt.Println("Error parsing rules: ", err)
@@ -134,23 +153,29 @@ func saveConfig(config Config) {
 	}
 }
 
-func handleRequest(conn net.Conn, index int) {
-	if Rules[index].Simultaneous != 0 && SimultaneousConnections[index] >= (Rules[index].Simultaneous*2) { //If we have reached quota just terminate the connection; 0 means no limits
+func handleRequest(conn net.Conn, index int, r Rule) {
+	//Send a clone of rules to here to avoid need of locking mutex
+	SimultaneousConnections.mu.Lock()
+	if r.Simultaneous != 0 && SimultaneousConnections.SimultaneousConnections[index] >= (r.Simultaneous*2) { //If we have reached quota just terminate the connection; 0 means no limits
 		if Verbose {
-			fmt.Println("Blocking new connection for port", Rules[index].Listen, "because the connection limit is reached. The current active connections count is", SimultaneousConnections[index]/2)
+			fmt.Println("Blocking new connection for port", r.Listen, "because the connection limit is reached. The current active connections count is", SimultaneousConnections.SimultaneousConnections[index]/2)
 		}
+		SimultaneousConnections.mu.Unlock()
 		_ = conn.Close()
 		return
 	}
+	SimultaneousConnections.mu.Unlock()
 
-	proxy, err := net.Dial("tcp", Rules[index].Forward) //Open a connection to remote host
+	proxy, err := net.Dial("tcp", r.Forward) //Open a connection to remote host
 	if err != nil {
 		println("Error on dialing remote host:", err.Error())
 		_ = conn.Close()
 		return
 	}
 
-	SimultaneousConnections[index] += 2 //Two is added; One for client to server and another for server to client
+	SimultaneousConnections.mu.Lock()
+	SimultaneousConnections.SimultaneousConnections[index] += 2 //Two is added; One for client to server and another for server to client
+	SimultaneousConnections.mu.Unlock()
 
 	go copyIO(conn, proxy, index)
 	go copyIO(proxy, conn, index)
@@ -159,7 +184,14 @@ func handleRequest(conn net.Conn, index int) {
 func copyIO(src, dest net.Conn, index int) {
 	defer src.Close()
 	defer dest.Close()
+
 	r, _ := io.Copy(src, dest) //r is the amount of bytes transferred
-	Rules[index].Quota -= r
-	SimultaneousConnections[index]-- //This will actually run twice
+
+	Rules.mu.Lock() //Lock to read the amount of data transferred
+	Rules.Rules[index].Quota -= r
+	Rules.mu.Unlock()
+
+	SimultaneousConnections.mu.Lock()
+	SimultaneousConnections.SimultaneousConnections[index]-- //This will actually run twice
+	SimultaneousConnections.mu.Unlock()
 }
